edcd9a0b63b2df8449b734a9b38fa9adb545aa09
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-21 19:16:24 +0200

    Initial Commit

---
+++ BackDropCarouselSUI/BackDropCarouselSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackDropCarouselSUIApp.swift
+//  BackDropCarouselSUI
+//
+//  Created by Kirill Khomicevich on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BackDropCarouselSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
